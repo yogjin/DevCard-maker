5a7e70d038dec1d079de7386c2ee18eb003bb5a7
yogjin <youngjin988@gmail.com>
2022-03-14 02:34:56 +0900

    feat(Card): Preview에 표시될 Card 컴포넌트 기본 틀 생성

---
+++ src/components/CardMaker/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+const Card = (props) => {
+  return (
+    <>
+      <div className={styles.image}>
+        <img alt="profile"></img>
+      </div>
+      <div className={styles.data}>
+        <span>name</span>
+        <span>company</span>
+        <div className={styles.divider}></div>
+        <span>title</span>
+        <span>email</span>
+        <span>message</span>
+        <div className={styles.skills}>skill img</div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
