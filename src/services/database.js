a5e68f689b2572c70c943468427e0a27b600b381
yogjin <youngjin988@gmail.com>
2022-03-15 03:18:46 +0900

    feat,refactor: DB write함수 추가, test 함수 삭제

--- src/services/database.js
+++ src/services/database.js
@@ -18,42 +18,10 @@ import { child, get, getDatabase, onValue, ref, set } from 'firebase/database';
 const db = getDatabase();
 
 // function for test
-export const pushData2 = (uid, changedData) => {
-  console.log('pushData2');
+export const pushData = (uid, changedData) => {
   set(ref(db, `users/${uid}`), {
     card: changedData,
   });
-};
-
-// function for test
-export const pushData = () => {
-  set(ref(db, `users/${'FeAYOxvRNHYv08GQPWvKyHborAC3'}`), {
-    card: {
-      company: 'CNU',
-      email: 'youngjin988@gmail.com',
-      message: 'go higher',
-      name: '영진',
-      skills: ['javascript', 'react'],
-      title: 'BackEnd Developer',
-    },
-  });
-};
-
-// function for test
-export const getData = async () => {
-  console.log('get');
-  await get(ref(db, `users/${'FeAYOxvRNHYv08GQPWvKyHborAC3'}`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-        return snapshot.val();
-      } else {
-        console.log('No data available');
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
 };
 
 // uid에 해당하는 card object 가져오기
