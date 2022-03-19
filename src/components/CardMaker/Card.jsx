21937e7d02d68ed0a3113b34e5bb1f8e58aa5219
yogjin <youngjin988@gmail.com>
2022-03-19 17:48:58 +0900

    style: Preview에 있는 Card style 대폭수정

--- src/components/CardMaker/Card.jsx
+++ src/components/CardMaker/Card.jsx
@@ -3,19 +3,40 @@ import styles from './Card.module.css';
 
 const Card = ({ card }) => {
   const { name, company, title, email, message } = card;
+  const leftCode = (
+    <pre>{`{  
+    ${title}
+    ${company}
+}
+
+
+<br/>
+<email/> ${email}
+<br/>
+<br/>
+<message/> ${message}
+`}</pre>
+  );
+  const rightCode = (
+    <pre>{`
+  <skills>
+  
+
+  </skills> 
+  `}</pre>
+  );
+
   return (
     <>
-      <div className={styles.image}>
-        <img alt="profile"></img>
+      <div className={styles.left}>
+        <div className={styles.name}>{name}</div>
+        <div>{leftCode}</div>
       </div>
-      <div className={styles.data}>
-        <span>{name}</span>
-        <span>{company}</span>
-        <div className={styles.divider}></div>
-        <span>{title}</span>
-        <span>{email}</span>
-        <span>{message}</span>
-        <div className={styles.skills}>skill img</div>
+      <div className={styles.right}>
+        <div className={styles.profile}>
+          <img src="./image/github.png" alt="profile"></img>
+        </div>
+        <div>{rightCode}</div>
       </div>
     </>
   );
