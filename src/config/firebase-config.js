aba7d66fa1efa606f0da70d98639d4d47d80b815
yogjin <youngjin988@gmail.com>
2022-05-30 13:56:58 +0900

    refactor: firebase config 중요 요소들을 .env에 저장

---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
